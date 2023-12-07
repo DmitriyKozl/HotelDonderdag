3acb2dd37bdaf9c15d32546c550c3f410336cc6f
Dmitriy Kozlov <dimitry_kozlov@hotmail.com>
2023-12-07 09:11:41 +0100

    registration

---
+++ Hotel.BL/Managers/RegistrationManager.cs
@@ -0,0 +1,16 @@
+﻿using Hotel.BL.Interfaces;
+using Hotel.BL.Model;
+
+namespace Hotel.Domain.Managers; 
+
+public class RegistrationManager {
+    private IRegistrationRepository registrationRepository;
+    public RegistrationManager(IRegistrationRepository registrationRepository)
+    {
+        this.registrationRepository = registrationRepository;
+    }
+    public void AddRegistration(Registration registration)
+    {
+        registrationRepository.AddRegistration(registration);
+    }
+}
---
+++ Hotel.Persistence/Repositories/RegistrationRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.SqlClient;
+using Hotel.BL.Interfaces;
+using Hotel.BL.Model;
+using HotelProject.DL.Exceptions;
+
+namespace Hotel.Persistence.Repositories;
+
+public class RegistrationRepository : IRegistrationRepository {
+    private string connectionString;
+
+    public RegistrationRepository(string connectionString) {
+        this.connectionString = connectionString;
+    }
+
+    public void AddRegistration(Registration registration) {
+        try {
+            string query =
+                "INSERT INTO Registrations (CustomerID, ActivityID, NumberOfAdults, NumberOfChildren,TotalCost) " +
+                "VALUES (@CustomerID, @ActivityID, @NumberOfAdults,@NumberOfChildren, @TotalCost)";
+            using (SqlConnection connection = new SqlConnection(connectionString)) {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@CustomerID", registration.Customer.Id);
+                command.Parameters.AddWithValue("@ActivityID", registration.Activity.ActivityId);
+                command.Parameters.AddWithValue("@NumberOfAdults", registration.NumberOfAdults);
+                command.Parameters.AddWithValue("@NumberOfChildren", registration.NumberOfChildren);
+                command.Parameters.AddWithValue("@TotalCost", registration.Price);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        catch (Exception e) {
+            throw new RegistrationRepositoryException("AddRegistration failed", e);
+        }
+    }
+}
